fc76f1fb013b7bf6a8559cd0ffc88517e82a28c4
ASwigart <andrew.swigart@gmail.com>
2022-10-12 19:29:04 -0500

    Wed

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # 
 import sqlite3
 import json
-import Pylance
+
 
 from flask import Flask, jsonify, g
 from flask_cors import CORS 
@@ -13,8 +13,12 @@ Database1 = '../resources/arrivals.sqlite'
 Database2 = '../resources/barchart.sqlite'
 Database3 = '../resources/world.sqlite'
 
+@app.route("/")
+# template for index.html 
+
 # https://flask.palletsprojects.com/en/2.2.x/patterns/sqlite3/
 # for arrivals
+@app.route("/api/v1.0/from80to2021")
 def connect_to_db():
     db1 = getattr(g, '_database1', None)
     if db1 is None:
@@ -27,12 +31,16 @@ def close_connection(exception):
     if db1 is not None:
         db1.close()
 # barchart 
+@app.route("/api/v1.0/individual-aslyum")
 
 def connect_to_db():
     db2 = getattr(g, '_database2', None)
     if db2 is None:
         db2 = g._database2 = sqlite3.connect('../resources/barchart.sqlite')
     return db2
+
+
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -41,12 +49,12 @@ def close_connection(exception):
         db2.close()
 
 # for cholopath
+@app.route("/api/v1.0/worldwide")
 def connect_to_db():
     db3 = getattr(g, '_database3', None)
     if db3 is None:
         db3 = g._database3 = sqlite3.connect('../resources/world.sqlite')
     return db3
-
 @app.teardown_appcontext
 def close_connection(exception):
     db3 = getattr(g, '_database3', None)

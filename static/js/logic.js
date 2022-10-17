e91e6c10907d5c8da66edba5c15b36291c2154ed
larsonki <kirsten.ifill.larson@gmail.com>
2022-10-17 12:52:38 -0500

    updated html and js for visualizations

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,108 +9,115 @@ function init() {
     // Create a variable for the drop-down and use D3 to select it from the html
     var dropDown = d3.select("#selDataset");
     // Fetch the JSON data
-    d3.json(url_arriv).then(function (data) {
+    d3.json(url_arriv).then(function (number) {
         // Create a new list for the years of arrival
+        var allYears = []
         var years = []
+        var arriv = []
         // Loop through the data and push the years into the empty list
-        for (var i=0; i < data.length; i++) {
-            var arr_year = data[i].year;
-            if (arr_year > 1989) {
+        for (var i=0; i < number.length; i++) {
+            var arr_year = number[i].year;
+            var arr = number[i].number;
+            if (arr_year > 2011) {
                 years.push(arr_year.toString());
             };
+            allYears.push(arr_year).toString();
+            arriv.push(arr);
         };
         // Confirm the loop pushed years into the list.
-        console.log(years);
+        console.log(arriv);
         // Add each year to the drop-down option
-        years.forEach((year) => {
-                dropDown.append("option").text(year).property("value", year)
-            // console.log(year)
+        rev_years = years.reverse()
+        rev_years.forEach((year) => {
+            dropDown.append("option").text(year).property("value", year)
+        // console.log(year)
             });
         // Review info in drop-down
         // console.log(dropDown)
-        var initYear = years[10];
+        var trace1 = {
+            x: allYears,
+            y: arriv
+        }
+
+        let data = [trace1];
+        let layout = {
+            title: "Total Arrivals by Year since 1980"
+        };
+        Plotly.newPlot("Arrivals", data, layout);
+        var initYear = rev_years[0];
         // buildWorld(initYear);
-        buildIndiv(initYear);
+        // buildIndiv(initYear);
     });
 
 };
 
-// // Build charts function using the sample from the initialize function to create each of the charts.
-function buildIndiv(year) {
-    d3.json(url_indiv).then(function (data) {
-        const labels = [
-            'Affirmative',
-            'Defensive'
-          ];
-        
-          const indiv = {
-            labels: labels,
-            datasets: [{
-              label: 'My First dataset',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: [0, 10, 5, 2, 20, 30, 45],
-            }]
-          };
-        
-          const config = {
-            type: 'line',
-            data: data,
-            options: {}
-          };
-        // Define variables needed for the charts
-        // var allSamples = data.samples;
-        // var sampleInfo = allSamples.filter(row => row.id == sample);
-        // var sampleValues = sampleInfo[0].sample_values;
-        // var sampleValuesSlice = sampleValues.slice(0,10).reverse();
-        // var otuIds = sampleInfo[0].otu_ids;
-        // var otuIdsSlice = otuIds.slice(0,10).reverse();
-        // var otuLabels = sampleInfo[0].otu_labels;
-        // var otuLabelsSlice = otuLabels.slice(0,10).reverse();
-        // var metaData = data.metadata;
-        // var metaDataSample = metaData.filter(row => row.id == sample);
-        // var wash = metaDataSample[0].wfreq;
+// Build a grouped bar chart for the types of asylees using a new JavaScript library - charts.js
+// Code adapted from Benjamin Carmichael at https://codepen.io/bencarmichael/pen/XeYJXJ
+d3.json(url_indiv).then(function (data) {
+       
+    const year = []
+    const affCount = []
+    const defCount = []
+    const total = []
+    for (var i=0; i < data.length; i++) {
+        var arr_year = data[i].year;
+        var arr_aff = data[i].affirmative;
+        var arr_def = data[i].defensive;
+        var arr_total = data[i].defensive;
+        year.push(arr_year.toString());
+        affCount.push(arr_aff);
+        defCount.push(arr_def);
+        total.push(arr_total);
+    };
+    
+    var barChartData = {
+        labels: year,
+        datasets: [
+            {
+                label: "Affirmative",
+                backgroundColor: "lightgreen",
+                borderColor: "green",
+                borderWidth: 1,
+                data: affCount
+            },
+            {
+                label: "Defensive",
+                backgroundColor: "lightblue",
+                borderColor: "blue",
+                borderWidth: 1,
+                data: defCount
+            }
+        ]
+    };
 
-        console.log(data);
+    var chartOptions = {
+        responsive: true,
+        legend: {
+            position: "top"
+        },
+        title: {
+            display: true,
+            text: "Number of Asylees by Year Based on Type"
+        }
+    };
+    window.onload = function() {
+        var ctx = document.getElementById("myChart").getContext("2d");
+        window.myBar = new Chart(ctx, {
+                type: "bar",
+                data: barChartData,
+                options: chartOptions
+        });
+    };
+console.log(defCount)
+});
 
-        // // Create the first chart (bar chart)
-        // var trace1 = {
-        //     x: sampleValuesSlice,
-        //     y: otuIdsSlice.map(item => `OTU ${item}`),
-        //     type: "bar",
-        //     orientation: "h",
-        //     text: otuLabelsSlice,
-        // };
-        // var data = [trace1];
-        // Plotly.newPlot("bar", data)
 
-        // // Create the second chart (bubble chart)
-        // var trace2 = {
-        //     x: otuIds,
-        //     y: sampleValues,
-        //     mode: "markers",
-        //     marker: {
-        //         size: sampleValues,
-        //         color: otuIds,
-        //         colorscale: "Earth"
-        //     },
-        //     text: otuIds
-        // };
-        // var data2 = [trace2];
-        // var layout = {
-        //     showlegend: false
-        // };
 
-        // Plotly.newPlot("bubble", data2, layout);
-
-    });
-};
-
-// // Option changed function pulled from HTML file and used to update build charts and build demo functions when the value in the drop-down
-// // changes
+// Option changed function pulled from HTML file and used to update build charts and build demo functions when the value in the drop-down
+// changes
 function optionChanged(year) {
     // buildWorld(year);
-    buildIndiv(year);
+    // buildIndiv(year);
 };
 
 // Calling the initialize function to run.
